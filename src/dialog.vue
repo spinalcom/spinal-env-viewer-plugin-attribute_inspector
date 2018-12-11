b4d505255e8d95139ed04301fa84fa8e7af257d7
alexis <alexis.auriac@epitech.eu>
2018-12-11 17:18:02 +0100

    Created app to create validation contexts

---
+++ src/dialog.vue
@@ -0,0 +1,76 @@
+<!--
+Copyright 2018 SpinalCom - www.spinalcom.com
+This file is part of SpinalCore.
+Please read all of the following terms and conditions
+of the Free Software license Agreement ("Agreement")
+carefully.
+This Agreement is a legally binding contract between
+the Licensee (as defined below) and SpinalCom that
+sets forth the terms and conditions that govern your
+use of the Program. By installing and/or using the
+Program, you agree to abide by all the terms and
+conditions stated or referenced herein.
+If you do not agree to abide by these terms and
+conditions, do not demonstrate your acceptance and do
+not install or use the Program.
+You should have received a copy of the license along
+with this file. If not, see
+<http://resources.spinalcom.com/licenses.pdf>.
+-->
+
+<template>
+  <md-dialog :md-active.sync="show"
+             @md-closed="closeDialog(false)">
+    <md-dialog-title>Create a Validation Context</md-dialog-title>
+
+    <md-dialog-content>
+      <md-field>
+        <label>Context Name</label>
+
+        <md-input v-model="contextName"></md-input>
+      </md-field>
+    </md-dialog-content>
+
+    <md-dialog-actions>
+      <md-button class="md-primary"
+                 @click="closeDialog(false)">
+        Close
+      </md-button>
+
+      <md-button class="md-primary"
+                 @click="closeDialog(true)">
+        Save
+      </md-button>
+    </md-dialog-actions>
+  </md-dialog>
+</template>
+
+<script>
+import { SpinalGraphService } from "spinal-env-viewer-graph-service";
+
+export default {
+  name: "create_validation_context_dialog",
+  props: ["onFinised"],
+  data() {
+    return {
+      show: true,
+      contextName: ""
+    };
+  },
+  methods: {
+    opened() {},
+    removed() {
+      this.show = false;
+    },
+    closeDialog(closeResult) {
+      this.contextName = this.contextName.trim();
+
+      if (closeResult && this.contextName !== "") {
+        SpinalGraphService.addContext(this.contextName, "validationContext");
+      }
+
+      this.onFinised({ inputValue: this.inputValue });
+    }
+  }
+};
+</script>
